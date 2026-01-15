df489d1bba3ffe594357f3ad2a4b2c97c5c4a329
Tamas Fehervari <58502181+zEdS15B3GCwq@users.noreply.github.com>
2026-01-15 23:49:06 +0900

    maint: add comments for main module and mining patch feature

--- TITweaksMod/Utils/SettingsUI.cs
+++ TITweaksMod/Utils/SettingsUI.cs
@@ -1,11 +1,13 @@
-﻿using PavonisInteractive.TerraInvicta.Actions;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityModManagerNet;
 
 namespace TITweaksMod
 {
     internal sealed class SettingsUIContext
     {
+        /// <summary>
+        /// Struct to hold common styles, layout options, and helper functions for the settings UI.
+        /// </summary>
         internal SettingsUIContext()
         {
             toggleStyle = new GUIStyle(GUI.skin.toggle) { contentOffset = new Vector2(8f, 0) };
@@ -24,6 +26,12 @@ namespace TITweaksMod
     {
         internal static SettingsUIContext? context { get; private set; }
 
+        /// <summary>
+        /// Handles drawing the mod settings UI via UMM, invoked at each redraw.
+        /// This method only draws the UI. Patch features define their own settings UI
+        /// sub-sections, which are included here.
+        /// </summary>
+        /// <param name="modEntry">UMM mod context</param>
         internal static void OnGUI(UnityModManager.ModEntry modEntry)
         {
             if (Main.Settings == null)
@@ -31,6 +39,7 @@ namespace TITweaksMod
 
             context ??= new SettingsUIContext();
 
+            /// Draw basic layout and title for the mod settings
             GUILayout.BeginVertical();
 
             GUILayout.Label($"{modEntry.Info.DisplayName} Mod Settings", UnityModManager.UI.h1);
@@ -41,6 +50,11 @@ namespace TITweaksMod
             GUILayout.EndVertical();
         }
 
+        /// <summary>
+        /// Called by UMM when the mod settings UI is closed/hidden.
+        /// Features define their own handlers, which are included here.
+        /// </summary>
+        /// <param name="modEntry">UMM mod context</param>
         internal static void OnHideGUI(UnityModManager.ModEntry modEntry)
         {
             if (Main.Settings == null)
